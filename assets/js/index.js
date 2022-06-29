9a80e2f2c526aae574d811c224d931498a72fb0b
Anaïs Bossouw <anais.bossouw@gmail.com>
2022-06-29 11:36:21 +0200

    presentation ok

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+(() => {
+
+    let gallery= [
+        "../../_shared/img/bell.svg",
+        "../../_shared/img/clock.svg",
+        "../../_shared/img/compass.svg",
+        "../../_shared/img/lemon.svg",
+        "../../_shared/img/map.svg",
+    ];
+
+    let i = 0;
+
+    document.getElementsByTagName("img")[0].id = "image";
+    let data = document.getElementById("image");
+
+    document.getElementById("next").addEventListener("click", () => {
+        data.setAttribute('src', gallery[i]);
+        i++;
+        if (i === gallery.length) {
+            i = 0;
+        }
+    });
+});
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Anaïs Bossouw">
+    <meta name="description" content="Portfolio">
+
+    <!--Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bellota&display=swap" rel="stylesheet"> 
+
+    <!--Style-->
+    <title>Portfolio Anaïs</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <!--Title Bar-->
+        <nav class="titleBar"> My Portfolio </nav>
+
+        <!--Block présentation-->
+        <div class="pack_presentation">
+            <div class="presentation">
+                <p class="pre">Welcome to my portfolio.
+                    <br>
+                    I'm Anaïs Bossouw.
+                    <br>
+                    I'm a junior front-end developer interested in UI and UX design.
+                </p>
+
+                <div class="rs_logo">
+                    <a href="https://github.com/Nymphadorart" target="_blank"><img src="assets/img/github.png"> </a>
+                    <a href="https://www.linkedin.com/in/anais-bossouw/" target="_blank"><img src="assets/img/linkedin.png"></a>
+                    <a href="mailto:anais.bossouw@gmail.com" target="_blank"><img src="assets/img/gmail.png"></a>
+                    <a href="https://www.instagram.com/nymphadora_rt/?hl=fr" target="_blank"><img src="assets/img/instagram.png"></a>
+                    <img src="assets/img/telephone.png">0494.451.870
+                </div>
+
+                <blockquote>
+                    <p>
+                        “It is not strength, but perseverance, 
+                        <br> 
+                        that makes great works.”
+                    </p>
+                    <cite>
+                        "Samuel Johnson"
+                    </cite>
+                </blockquote>
+            </div>
+
+            <div class="avatar">
+                <img src="assets/img/avatar.png" alt="avatar">
+            </div>
+        </div>
+    </header>
+
+    <!--My projects-->
+    <nav class="titleBar"> My Projects </nav>
+
+    
+    
+</body>
+</html>
